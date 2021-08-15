1f8e64750faffe91b29d055599183686605e4dc8
Rubén Medellín <ruben.medellin.c@gmail.com>
2021-08-15 15:35:31 -0500

    LiveMap added

--- client/src/components/livemap/LiveMap.js
+++ client/src/components/livemap/LiveMap.js
@@ -1,8 +1,39 @@
+import React, { useEffect, useRef, useState } from 'react'
+import mapboxgl from 'mapbox-gl'
+import 'mapbox-gl/dist/mapbox-gl.css'
+
+const styles = {
+  width: '100vw',
+  position: 'absolute'
+}
+
 const LiveMap = () => {
+
+  const [map, setMap] = useState(null);
+  const mapContainer = useRef(null);
+
+  useEffect(() => {
+    mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_KEY;
+    const initializeMap = ({ setMap, mapContainer }) => {
+      const map = new mapboxgl.Map({
+        container: mapContainer.current,
+        style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+        center: [0, 0],
+        zoom: 5
+      });
+
+      map.on('load', () => {
+        setMap(map)
+        map.resize()
+      })
+    }
+
+    if (!map) initializeMap({ setMap, mapContainer })
+  }, [map])
+
   return (
-    <div className="container mx-auto py-10 h-64 md:w-4/5 w-11/12 px-6">
-      {/* Remove class [ border-dashed border-2 border-gray-300 ] to remove dotted border */}
-      <div className="w-full h-full rounded border-dashed border-2 border-gray-300">{/* Place your content here */}</div>
+    <div className="container h-64 md:h-full">
+      <div className="w-full h-full rounded" ref={el => (mapContainer.current = el)} style={styles} />
     </div>
   )
 }
